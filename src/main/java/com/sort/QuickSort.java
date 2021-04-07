bd8e40f86db2af7bd8061147c771b10d2f3fa1e9
dreamren <abwrt@163.com>
2021-04-07 17:59:08 +0800

    快拍

--- src/main/java/com/sort/QuickSort.java
+++ src/main/java/com/sort/QuickSort.java
@@ -4,42 +4,50 @@ import java.io.PrintWriter;
 
 public class QuickSort {
 
+	public static void main(String[] args) {
+		int[] arr = new int[]{6,2,8,12,5,3,8,1,4};
+		quickSort(arr,0,arr.length-1);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
 
 
 	
 	
-	  public static void quickSort(int[] a,int low,int high){
+	  public static void quickSort(int[] arr,int low,int high){
 	         int start = low;
 	         int end = high;
-	         int key = a[low];
-	         
+	         int key = arr[low];
+		  System.out.println("key:"+key);
 	         
 	         while(end>start){
 	             //从后往前比较
-	             while(end>start&&a[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+	             while(end>start&&arr[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
 	                 end--;
-	             if(a[end]<=key){
-	                 int temp = a[end];
-	                 a[end] = a[start];
-	                 a[start] = temp;
+	             if(arr[end]<=key){
+	                 int temp = arr[end];
+	                 arr[end] = arr[start];
+	                 arr[start] = temp;
 	             }
 	             //从前往后比较
-	             while(end>start&&a[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+	             while(end>start&&arr[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
 	                start++;
-	             if(a[start]>=key){
-	                 int temp = a[start];
-	                 a[start] = a[end];
-	                 a[end] = temp;
+	             if(arr[start]>=key){
+	                 int temp = arr[start];
+	                 arr[start] = arr[end];
+	                 arr[end] = temp;
 	             }
 	         //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
-	             for (int num : a) {
+	             for (int num : arr) {
 	 				System.out.print(num+" ");
 	 			}
 	             System.out.println();
 	         }
 	         //递归
-	         if(start>low) quickSort(a,low,start-1);//左边序列。第一个索引位置到关键值索引-1
-	         if(end<high) quickSort(a,end+1,high);//右边序列。从关键值索引+1到最后一个
+	         if(start>low) quickSort(arr,low,start-1);//左边序列。第一个索引位置到关键值索引-1
+	         if(end<high) quickSort(arr,end+1,high);//右边序列。从关键值索引+1到最后一个
 	     }
 	
 	  
--- src/main/java/com/thread/Test.java
+++ src/main/java/com/thread/Test.java
@@ -1,5 +1,6 @@
 package com.thread;
 
+import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,15 +8,26 @@ import com.iq.Person;
 
 public class Test {
 
+
+
 	public Test() {
 		System.out.println("无参构造方法");
 	}
 	public void Test() {
 		System.out.println("Test方法");
 	}
+
 	   public static void main(String[] args) {
-		   Test test = new Test();
-		   test.Test();
+//		   Test test = new Test();
+//		   test.Test();
+		   float f1 = 0.1f;
+		   float f2 = 0.2f;
+		   System.out.println(f1+f2);
+		   double d1 = 0.1;
+		   double d2 = 0.2;
+		   System.out.println(d1+d2);
+		   String s = "16602102355";
+		   System.out.println(MessageFormat.format("mcht:verification:code:{0}", "+8616602102355"));
 	    }
 	
 	
